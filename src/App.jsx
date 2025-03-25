8ce89be0d693adc42402333b20fbf7d4211edfeb
saravana7447 <s4tech1234@gmail.com>
2025-03-25 19:26:58 +0530

    Register page for student created

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Dashboard from "./Components/Dashboard";
 import Feepayment from "./Pages/Fee/Feepayment";
 import Circular from "./Pages/Circular/Circular";
 import Attendance from "./Pages/Attendance/Attendance";
+import Registerpage from "./Loginpage/registerpage";
 
 function App() {
   return (
     <>
-        <Attendance/>
+        <Registerpage/>
     </>
   );
 }
