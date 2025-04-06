ecbd1ec6a18b446c3c55f5a3b1987d802b4cb77a
SuviSharon <suvisharon79@gmail.com>
2025-04-06 15:42:13 +0530

    Solved Case issues

--- Backend/Routes/courseRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { addCourse, getCourse } from '../Controllers/courseController.js';
-const router = express.Router();
-router.post('/' , addCourse)
-router.get('/:id' , getCourse);
-export default router;
--- backend/models/index.js
+++
@@ -1,8 +0,0 @@
-import Student from './student';
-import Contact from './contact';
-
-
-Student.hasMany(Contact, { foreignKey: 'student_id' });
-Contact.belongsTo(Student, { foreignKey: 'student_id' });
-
-export { Student, Contact };
